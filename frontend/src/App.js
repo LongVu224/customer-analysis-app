f3092abe06cef42ac3410d160fabdedaa92ff0ec
longvu <174439563+v-vulong_microsoft@users.noreply.github.com>
2025-05-31 16:36:41 +0300

    navigation menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ScrollToTop from './components/helper/ScrollToTop/ScrollToTop';
+import Burger from './components/Navigation/Burger';
+import Menu from './components/Navigation/Menu';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Insights from './routes/Insights';
 
+const useOnClickOutside = (ref, handler) => {
+  React.useEffect(() => {
+    const listener = event => {
+      if (!ref.current || ref.current.contains(event.target)) return;
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [ref, handler]);
+};
+
 function App() {
+  const [open, setOpen] = React.useState(false);
+  const node = React.useRef();
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
     <div>
       <ScrollToTop />
@@ -12,6 +32,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Insights" element={<Insights />} />
       </Routes>
+      <div ref={node}>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu open={open} setOpen={setOpen} />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/Navigation/Menu.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #f5f5f5; /* light grey */
+  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
+  height: 100vh;
+  text-align: left;
+  padding: 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: transform 0.3s ease-in-out;
+
+  @media (max-width: 576px) {
+    width: 100%;
+  }
+
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: #0d0c1d;
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    &:hover {
+      color: #343078;
+    }
+  }
+`;
+
+const Menu = ({ open }) => {
+  return (
+    <StyledMenu open={open}>
+      <a href="/">
+        Upload
+      </a>
+      <a href="/Insights">
+        Insights
+      </a>
+    </StyledMenu>
+  );
+};
+
+export default Menu;
