feed88e8b9033b6e682f3bdf50c01388a300d1b9
longvu <174439563+v-vulong_microsoft@users.noreply.github.com>
2025-06-12 09:14:24 +0300

    monitor service

---
+++ backend/monitor-service/models/ProcessLog.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const logSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    serviceName: String,
+    processName: String,
+    message: String,
+    date: String
+}, {
+    collection: 'processLogs'
+})
+
+const saveProcessLog = async (serviceName, processName, message) => {
+    const logEntry = new logSchema({
+      _id: new mongoose.Types.ObjectId(),
+      serviceName: serviceName,
+      processName: processName,
+      message: message,
+      date: new Date().toISOString()
+    });
+
+    try {
+        await logEntry.save();
+    } catch (error) {
+        console.error('Error saving log:', error);
+    }
+}
+
+module.exports = {
+    ProcessLog: mongoose.model('ProcessLog', logSchema),
+    saveProcessLog: saveProcessLog
+};
---
+++ backend/monitor-service/routes/monitor.routes.js
@@ -0,0 +1,42 @@
+const express = require('express'),
+  mongoose = require('mongoose'),
+  monitorApi = express.Router();
+
+// file model
+const { saveProcessLog, ProcessLog } = require('../models/ProcessLog');
+
+// env configuration
+const config = require("../config/config");
+
+monitorApi.get('/', async (req, res) => {
+  ProcessLog.find()
+    .then(logs => {
+      res.status(200).json({
+        message: "Logs fetched successfully",
+        logs: logs
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error fetching logs",
+        error: err.message
+      });
+    });
+});
+
+monitorApi.get('/service/:serviceName', async (req, res) => {
+  const serviceName = req.params.serviceName;
+  ProcessLog.find({ serviceName: serviceName })
+    .then(logs => {
+      res.status(200).json({
+        message: `Logs fetched successfully for service: ${serviceName}`,
+        logs: logs
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error fetching logs",
+        error: err.message
+      });
+    });
+});
